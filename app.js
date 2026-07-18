860221d7912ed63d3f3da99c1efbcc08a9ecb8ac
ryoi1d8c0 <ryo1d8c0@gmail.com>
2026-07-18 12:46:06 +0900

    認証の基本的な機能を追加

--- app.js
+++ app.js
@@ -6,21 +6,25 @@ const session = require('express-session');
 const flash = require('connect-flash');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
+const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp',
-    {
+mongoose
+    .connect('mongodb://localhost:27017/yelp-camp', {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useCreateIndex: true,
-        useFindAndModify: false
+        useFindAndModify: false,
     })
     .then(() => {
         console.log('MongoDBコネクションOK！！');
     })
-    .catch(err => {
+    .catch((err) => {
         console.log('MongoDBコネクションエラー！！！');
         console.log(err);
     });
@@ -33,7 +37,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
 const sessionConfig = {
     secret: 'mysecret',
@@ -41,13 +45,22 @@ const sessionConfig = {
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
-        maxAge: 1000 * 60 * 60 * 24 * 7
-    }
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+    },
 };
 app.use(session(sessionConfig));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use(flash());
 
 app.use((req, res, next) => {
+    console.log(req.session);
+    res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
@@ -57,6 +70,7 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
+app.use('/', userRoutes);
 app.use('/campgrounds', campgroundRoutes);
 app.use('/campgrounds/:id/reviews', reviewRoutes);
 
@@ -67,7 +81,7 @@ app.all('*', (req, res, next) => {
 app.use((err, req, res, next) => {
     const { statusCode = 500 } = err;
     if (!err.message) {
-        err.message = '問題が起きました'
+        err.message = '問題が起きました';
     }
     res.status(statusCode).render('error', { err });
 });
